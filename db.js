a6027057d42885b6ad6407edd79c78d5a540bb56
fayzanrj <121216897+fayzanrj@users.noreply.github.com>
2023-04-06 02:23:08 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-require("dotenv").config()
-
 // const mongoURI = process.env.MONGO_URL;
 // const mongoURI = 'mongodb://127.0.0.1:27017/inotebook';
 const mongoURI = "mongodb+srv://fayzanrj786:a4b65595@cluster0.k5kzu9p.mongodb.net/?retryWrites=true&w=majority"

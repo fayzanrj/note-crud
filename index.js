e58934da1c36aa01d0d3777ec066bd5836747ad9
fayzanrj <121216897+fayzanrj@users.noreply.github.com>
2023-04-06 03:09:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use('/api/auth' , require('./routes/auth'))
 app.use('/api/notes' , require('./routes/notes'))
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })

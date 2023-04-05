5883818f6b7ca75a8b83603b32c8ef0abf3b26fb
fayzanrj <121216897+fayzanrj@users.noreply.github.com>
2023-04-06 02:30:17 +0500

    Add files via upload

---
+++ routes/fetchUser.js
@@ -0,0 +1,20 @@
+var jwt = require('jsonwebtoken');
+require("dotenv").config()
+
+let JWT_Secret = process.env.JWT_SECRET;
+
+const fetchUser = (req, res, next) => {
+    const token = req.header('authToken');
+    if (!token) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+    try {
+        const data = jwt.verify(token, JWT_Secret)
+        req.user = data.user;
+        next()
+    } catch (error) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+}
+
+module.exports = fetchUser; 
